a6ae3ba225c09fd8c647c1e18a250e0ad1d026d5
Fayaaz Ahmed <fayaaz.ahmed@gmail.com>
2020-11-17 02:51:17 +0000

    Try and build mixxx ourselves

--- stage3/01-install-packages/01-run.sh
+++ stage3/01-install-packages/01-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 ## Build Mixxx
+<<<<<<< HEAD
 mkdir -p ${BASE_DIR}/.ccache/
 mkdir -p "${ROOTFS_DIR}/ccache"
 mount --bind ${BASE_DIR}/.ccache  "${ROOTFS_DIR}/ccache"
@@ -28,3 +29,21 @@ unmount "${BASE_DIR}/.ccache"
 mkdir -p "$DEPLOY_DIR"
 cp ${ROOTFS_DIR}/code/build/*.deb "$DEPLOY_DIR/"
 rm -rf ${ROOTFS_DIR}/code/build/CMakeFiles/
+=======
+on_chroot << EOF
+    git clone https://github.com/mixxxdj/mixxx.git /code/
+    cd /code/ && git checkout 2.3
+    export CTEST_PARALLEL_LEVEL="$(nproc)"
+    export CMAKE_BUILD_PARALLEL_LEVEL="$(nproc)"
+    export PATH="$HOME/.local/bin:$PATH"
+    export CMAKEFLAGS="-DCMAKE_BUILD_TYPE=Release -DBATTERY=ON -DBROADCAST=ON -DBULK=ON -DDEBUG_ASSERTIONS_FATAL=ON -DHID=ON -DLILV=ON -DOPUS=ON -DQTKEYCHAIN=ON -DVINYLCONTROL=ON -DENGINEPRIME=ON"
+    export CMAKEFLAGS_EXTRA="-DFAAD=ON -DKEYFINDER=ON -DLOCALECOMPARE=ON -DMAD=ON -DMODPLUG=ON -DWAVPACK=ON -DWARNINGS_FATAL=ON"
+    export GTEST_COLOR="1"
+    export CTEST_OUTPUT_ON_FAILURE="1"
+    export QT_QPA_PLATFORM="offscreen"
+    mkdir cmake_build && cd cmake_build
+    cmake -DCMAKE_INSTALL_PREFIX=/usr .. 
+    cmake -L $CMAKEFLAGS $CMAKEFLAGS_EXTRA ..
+    cmake --build . --target install
+EOF
+>>>>>>> 52bb35d... Try and build mixxx ourselves
