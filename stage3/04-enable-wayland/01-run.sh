3898ee9c5378a5ae9d612d565ebe19daf1d4a87e
Fayaaz Ahmed <fayaaz.ahmed@censornet.com>
2023-12-23 16:47:21 +0000

    Remove packages and mask services

--- stage3/04-enable-wayland/01-run.sh
+++ stage3/04-enable-wayland/01-run.sh
@@ -1,12 +1,25 @@
+# Enable wayland
 on_chroot << EOF
 	SUDO_USER=pi raspi-config nonint do_boot_behaviour B4
 	raspi-config nonint do_xcompmgr 0
 	SUDO_USER=pi raspi-config nonint do_wayland W2
 EOF
 
+# Remove cups
 on_chroot << EOF
-    systemctl --user -M pi@ mask pipewire
-    systemctl --user -M pi@ mask pipewire-pulse
-    apt-get purge -y cups cups-common
+    apt-get purge -y cups cups-common system-config-printer printer-driver-* pocketsphinx-* pi-printer-support
     apt-get autoremove -y
+EOF
+
+# Mask pipewire services
+on_chroot << EOF
+    systemctl mask pipewire
+    systemctl mask pipewire-pulse
+    systemctl mask wireplumber
+    su pi
+    systemctl --user mask pipewire
+    systemctl --user mask pipewire.socket
+    systemctl --user mask pipewire-pulse
+    systemctl --user mask pipewire-pulse.socket
+    systemctl --user mask wireplumber
 EOF
