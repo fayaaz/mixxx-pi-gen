752790415fa5bc5457893a7bfae172044d2c4023
Fayaaz Ahmed <fayaaz@users.noreply.github.com>
2025-09-12 21:02:14 +0100

    install performance governor file

--- stage3/04-enable-wayland/02-run.sh
+++ stage3/04-enable-wayland/02-run.sh
@@ -5,3 +5,7 @@ on_chroot << EOF
     chown -R pi:root /home/pi/.config/
     chmod -R 755 /home/pi/.config/
 EOF
+
+# Set performance governor
+rm ${ROOTFS_DIR}/usr/lib/udev/rules.d/60-ondemand-governor.rules
+install -m 644 files/60-performance-governor.rules ${ROOTFS_DIR}/usr/lib/udev/rules.d/60-performance-governor.rules
