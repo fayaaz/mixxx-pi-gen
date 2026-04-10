91ed0bf86e850b398a4481a4e78b549b8e5fdee3
Fayaaz <fayaaz.ahmed@gmail.com>
2026-04-10 02:11:37 +0100

    Fix mixxx alerts hidden behind fullscreen with auto-exit script

--- stage3/02-desktop/files/waybar/mixxxnowplaying.sh
+++ stage3/02-desktop/files/waybar/mixxxnowplaying.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+PORT=8080
+
 case "$1" in
     status)
-	if [[ $(lsof -i tcp:8080) ]]; then
-          echo "Running"
+        if ss -tln | grep -q ":$PORT "; then
+            echo "Running"
         else
-          echo "Stopped"
+            echo "Stopped"
         fi
-     ;;
-     start)
-	if [[ $(lsof -i tcp:8080) ]]; then
-	  exit 0
-	fi
+        ;;
+    start)
+        if ss -tln | grep -q ":$PORT "; then
+            exit 0
+        fi
 
-	while true;
-	   do a=$(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "org.mixxx.Mixxx") | select(.name | test("Mixxx")) | .name // empty' | cut -d '|' -f1 )
-	   title=${a/%"Mixxx"}
-	   echo $title
-	   echo -e "HTTP/1.1 200 OK\n\n$title" | nc -N -l -p 8080 ;
-	done
-	;;
+        while true; do
+            title=$(swaymsg -t get_tree | jq -r '.. | select(.app_id? == "org.mixxx.Mixxx") | .name' | head -1)
+            title="${title%Mixxx}"
+            echo "$title"
+            echo -e "HTTP/1.1 200 OK\n\n$title" | nc -N -l -p $PORT
+        done
+        ;;
 esac
