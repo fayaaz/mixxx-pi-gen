a35877b33be75129ce882e2a5e56514a386f262e
Fayaaz <fayaaz.ahmed@gmail.com>
2026-04-10 02:11:37 +0100

    Fix mixxx alerts hidden behind fullscreen with auto-exit script

--- stage3/02-desktop/02-run.sh
+++ stage3/02-desktop/02-run.sh
@@ -6,6 +6,8 @@ install -m 644 files/xdg-desktop-portal-sway.conf ${ROOTFS_DIR}/home/pi/.config/
 mkdir -p -m 755 ${ROOTFS_DIR}/home/pi/.config/systemd/user/
 install -m 644 files/systemd/user/sway-session.service ${ROOTFS_DIR}/home/pi/.config/systemd/user/
 install -m 644 files/systemd/user/sway-session.target ${ROOTFS_DIR}/home/pi/.config/systemd/user/
+mkdir -p -m 755 ${ROOTFS_DIR}/home/pi/.config/sway/scripts/
+install -m 755 files/sway/scripts/auto_exit_fullscreen.sh ${ROOTFS_DIR}/home/pi/.config/sway/scripts/
 cp -r files/wallpaper ${ROOTFS_DIR}/home/pi/
 cp -r files/i3blocks ${ROOTFS_DIR}/home/pi/.config/
 cp -r files/waybar  ${ROOTFS_DIR}/home/pi/.config/
